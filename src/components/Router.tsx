edbd53f809f8298f322e04ea2095936228956731
Jose Velasquez <velasquezdenorajose@gmail.com>
2025-08-15 17:29:50 -0400

    feat: Implements react router

---
+++ src/components/Router.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { QuizForm } from "@/components/quiz/QuizForm";
+import { QuizQuestion } from "@/components/quiz/QuizQuestion";
+
+export function Router() {
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<QuizForm />} />
+				<Route path="/quiz" element={<QuizQuestion />} />
+				<Route path="/results" element={<div>Result Page</div>} />
+			</Routes>
+		</BrowserRouter>
+	);
+}
