f0723d2e1833f7f69952bc6fad8a7bd200fdad99
Jose Velasquez <velasquezdenorajose@gmail.com>
2025-08-15 17:30:21 -0400

    feat: Add types for requests

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -3,3 +3,31 @@ export interface QuizInfo {
 	level: string;
 	language: string;
 }
+
+export interface GeneratedQuiz {
+	topic: string;
+	questions: Question[];
+}
+
+export interface Question {
+	id: number;
+	question: string;
+	options: string[];
+}
+
+export interface QuizResults {
+	total: number;
+	correct: number;
+	incorrect: number;
+	percentage: number;
+	results: VerifiedQuestion[];
+}
+
+export interface VerifiedQuestion {
+	id: number;
+	question: string;
+	isCorrect: boolean;
+	userAnswer: string;
+}
+
+export type UserAnswers = Record<string, string>;
